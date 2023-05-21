2722e300b9848deab3953850205f16c330b4e130
Rhys <rhysapfelbaum@gmail.com>
2023-05-22 00:34:15 +0100

    Fix zero padding bug

--- data_in/generator.py
+++ data_in/generator.py
@@ -1,17 +1,15 @@
 from struct import iter_unpack
 
 OUTPUT_PATH = r'minecraft_bitcoin_miner\data_in\page_sequence.txt'
-chunk = b'\x12\x34\xab\xcd'
+chunk = b'\x00\xff\x00\xff'
 
 message_words = (i[0] for i in iter_unpack('>I', chunk))
 
 with open(OUTPUT_PATH, 'w') as f:
     f.write(
         ','.join(
-            ('{:x}'.format(word) for word in message_words)
+            ('{:08x}'.format(word) for word in message_words)
         )
     )
     for word in message_words:
         f.write()
-
-    
